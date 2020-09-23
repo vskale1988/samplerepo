7a7ce5c298ddd6fb104029f48518219ca60721e3
vskale1988 <69645827+vskale1988@users.noreply.github.com>
2020-09-23 22:03:57 +0530

    Update SampleApplication.java
    remove space

--- src/main/java/com/sample/SampleApplication.java
+++ src/main/java/com/sample/SampleApplication.java
@@ -2,7 +2,6 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 //add to master branch 
 @SpringBootApplication
 public class SampleApplication {
