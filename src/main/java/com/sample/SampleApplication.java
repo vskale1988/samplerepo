21ec169351f8b428ea41e018c607bb6affabea2e
vskale <vskalepatil@gmail.com>
2020-09-23 17:29:58 +0530

    initial commit"

---
+++ src/main/java/com/sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//add to master branch
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
